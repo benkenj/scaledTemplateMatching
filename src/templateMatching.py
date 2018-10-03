cc4af9a244fbd215951332518e9eb7e1aad02490
benkenj <benkenjim@gmail.com>
2018-10-02 18:27:59 -0700

    clean up, separate into more isolated code incase someone wants the array or coordinates for template matching

--- src/templateMatching.py
+++ src/templateMatching.py
@@ -6,6 +6,10 @@ import ncc
 
 TEMPLATE_WIDTH = 15
 
+# Create a image pyramid
+# input: image: image to make a pyramid
+# input: minsize: min size of an image in pyramid
+# output: image pyramid in the form of an array of PIL images
 def MakePyramid(image, minsize):
     pyramid = [image]
     x,y = image.size
@@ -16,6 +20,7 @@ def MakePyramid(image, minsize):
         
     return pyramid
 
+# display the pyramid for testng purposes
 def ShowPyramid(pryamid):
     width = []
     height = pyramid[0].size[1]
@@ -27,6 +32,7 @@ def ShowPyramid(pryamid):
         combined_pyramid.paste(pryamid[i],(sum(width[0:i]),0))
     combined_pyramid.show()
 
+
 def drawRect(x,y, halfwidth, halfheight, im):
     draw = ImageDraw.Draw(im)
    
@@ -36,26 +42,46 @@ def drawRect(x,y, halfwidth, halfheight, im):
     draw.line((x-halfwidth,y-halfheight,x+halfwidth,y-halfheight),fill=200,width=2)
     del draw
 
-
-def FindTemplate(pyramid, template, threshold):
-
+# Create an array of coordinates where each coordinate is the location matching the template
+# input: pyramid: a pyramid of images
+# input: template: the image to be matched 
+# input: threshold: the min value for a template to match
+# output: an array or tupes matching templates
+def GetMatchedTemplateCoordinates(pyramid, template, threshold):
     x,y = template.size
     xyratio = float(y)/float(x)
     normtemplate = template.resize((TEMPLATE_WIDTH,int(float(TEMPLATE_WIDTH)*(xyratio))), Image.BICUBIC)
-    corr = []
-    highvals = []
+    # an array of normalized correlations for each image in the pyramid
+    correlatedarray = []
+    # an array of tuples (x,y,index) where index is the index in the pyramid 
+    # this is in order to kow how much to scale the template later
+    matchingvalues = []
     for i in range(0,len(pyramid)):
-        corr.append(ncc.normxcorr2D(pyramid[i], normtemplate))     
-        for x in range(0, corr[i].shape[0]):
-            for y in range(0, corr[i].shape[1]):
-                denormal = corr[i][x][y]
-                if corr[i][x][y] > float(threshold):          
+        # get correlation result from template and an image in the pryamid
+        correlatedarray.append(ncc.normxcorr2D(pyramid[i], normtemplate))     
+        for x in range(0, correlatedarray[i].shape[0]):
+            for y in range(0, correlatedarray[i].shape[1]):
+                denormal = correlatedarray[i][x][y]
+                # if we have value above the threshold, we add that coorinate to the array for later
+                if correlatedarray[i][x][y] > float(threshold):          
                     scalefactor = 1.0/pow(.75,i)            
-                    highvals.append((int(float(x)*scalefactor),int(float(y)*scalefactor), i))
+                    matchingvalues.append((int(float(x)*scalefactor),int(float(y)*scalefactor), i))
+    return matchingvalues
 
+
+# Given a pyramid, template and threshold, display where the template matches
+# input: pyramid: a pyramid of images
+# input: template: the image to be matched 
+# input: threshold: the min value for a template to match
+def FindTemplate(pyramid, template, threshold):  
+    x,y = template.size
+    xyratio = float(y)/float(x)
+    highvals = GetMatchedTemplateCoordinates(pyramid, template, threshold)
+    #draw boxes around faces
     for i in range(0,len(highvals)):
         scalefactor = 1.0/pow(.75,highvals[i][2])
         drawRect(highvals[i][1],highvals[i][0], int((float(TEMPLATE_WIDTH)*scalefactor)/2.0), int((float(TEMPLATE_WIDTH)*(xyratio)*scalefactor)/2.0),pyramid[0])
+    #display the image
     pyramid[0].show()
 
 img_path = "faces/judybats.jpg"
